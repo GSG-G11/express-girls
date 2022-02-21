6656f4b83f569b6417f8f2035596e6ee47b3699b
Fady-Alwazir <fady.alwazir@gmail.com>
2022-02-21 23:49:37 +0200

    update

--- public/script.js
+++ public/script.js
@@ -1,35 +1,21 @@
-document.onreadystatechange = () => {
-  if (document.readyState === 'complete') {
-    const xhr = new XMLHttpRequest();
+fetch('/posts')
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    for (let blogPost in data) {
+      const postDiv = document.createElement('div');
+      const postText = document.createElement('p');
+      const thumbnail = document.createElement('img');
+      const postContainer =
+        document.getElementsByClassName('post-container')[0];
 
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          const data = JSON.parse(xhr.responseText);
-          console.log(data)
-          for (let blogPost in data) {
-            const postDiv = document.createElement('div');
-            const postText = document.createElement('p');
-            const thumbnail = document.createElement('img');
-            const postContainer = document.getElementsByClassName(
-              'post-container'
-            )[0];
+      thumbnail.src = 'img/logo2.png';
+      thumbnail.className = 'thumbnail';
+      postText.innerHTML = data[blogPost];
+      postDiv.className = 'post';
 
-            thumbnail.src = 'img/logo2.png';
-            thumbnail.className = 'thumbnail';
-            postText.innerHTML = data[blogPost];
-            postDiv.className = 'post';
-
-            postDiv.appendChild(thumbnail);
-            postDiv.appendChild(postText);
-            postContainer.appendChild(postDiv);
-          }
-        } else {
-          console.error(xhr.responseText);
-        }
-      }
-    };
-    xhr.open('GET', '/posts', true);
-    xhr.send();
-  }
-};
+      postDiv.appendChild(thumbnail);
+      postDiv.appendChild(postText);
+      postContainer.appendChild(postDiv);
+    }
+  });
--- src/app.js
+++ src/app.js
@@ -1,38 +1,46 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
-const fs = require("fs");
-app.use(express.static("public"));
+const path = require('path');
+const fs = require('fs');
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.sendfile(path.join(__dirname, "..", "public", "index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
-app.post("/create-post", (req, res) => {
-  const filePath = path.join(__dirname, "posts.json");
+app.post('/create-post', (req, res) => {
+  const filePath = path.join(__dirname, 'posts.json');
 
   fs.readFile(filePath, (error, data) => {
     if (error) {
       console.log(error);
     } else {
       let obj = JSON.parse(data);
-      console.log("the req body", req.body.post);
+      console.log('the req body', req.body.post);
       obj[Date.now()] = req.body.post;
-      console.log("my obj is", obj);
-      ///fs.writeFile( filePath, JSON.stringify(obj),()=>{})
+      console.log('my obj is', obj);
 
-      fs.writeFile(filePath, JSON.stringify(obj), error => {
+      fs.writeFile(filePath, JSON.stringify(obj), (error) => {
         if (error) {
         } else {
-          res.redirect("/");
         }
       });
     }
   });
+  res.redirect('/');
+  ///// ??????????????????
 });
-app.get("/posts", (req, res) => {});
+app.get('/posts', (req, res) => {
+  fs.readFile(path.join(__dirname, 'posts.json'), (error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(data);
+    }
+  });
+});
 
 app.listen(4000, () => {
-  console.log("I am listening in port 4000");
+  console.log('I am listening in port 4000');
 });
